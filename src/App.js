48b652879ea27f90bac90677f066c63d9d652a68
Oleksii26 <Kolesnik26@ukr.net>
2022-11-25 18:25:13 +0200

    save

---
+++ src/App.js
@@ -0,0 +1,85 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import css from './App.module.css'
+import image from './img/gny.png'
+import image1 from './img/gny1.png'
+
+const getData = async () => {
+  const { data } = await axios.get('https://russianwarship.rip/api/v1/statistics/latest')
+  return data
+}
+
+const App = () => {
+  const [state, setStae] = useState([])
+
+  useEffect(() => {
+    getData().then(data => setStae(data.data))
+     }, [])
+
+  const { increase, stats } = state
+console.log(state)
+console.log('state')
+
+  return (<div className={css.heroes}>
+    <div className={css.anim}>
+    <h4 className={css.header}>Russian warship, go fuck yourself! Русский военный корабль, пошел на ...й! Російський воєнний корабель, іди на ...й!</h4>
+    </div>
+    <div className={css.container}>
+      <p className={css.gsy}>Генеральний штаб ЗС України інформує</p>
+      <h3>Загальні бойові втрати російського окупанта</h3>
+      <p><span className={css.span}>станом на {state.date}</span> ({state.day}-й день війни)</p>
+      <div className={css.table}>
+        <div className={css.td}>
+          <h4>{stats ? stats.personnel_units : '0'}(+{increase ? increase.personnel_units : 0})</h4>
+          <p>особового складу</p></div>
+        <div className={css.td}>
+          <h4>{stats ? stats.tanks : '0'}(+{increase ? increase.tanks : 0})</h4>
+          <p>танків</p></div>
+        <div className={css.td}>
+          <h4>{stats ? stats.armoured_fighting_vehicles : '0'}(+{increase ? increase.armoured_fighting_vehicles : 0})</h4>
+          <p>БМП</p></div>
+        <div className={css.td}>
+          <h4>{stats ? stats.artillery_systems : '0'}(+{increase ? increase.artillery_systems : 0})</h4>
+          <p>артилерійських систем</p></div>
+        <div className={css.td}>
+          <h4>{stats ? stats.mlrs : '0'}(+{increase ? increase.mlrs : 0})</h4>
+          <p>РСЗВ</p></div>
+        <div className={css.td}>
+          <h4>{stats ? stats.aa_warfare_systems : '0'}(+{increase ? increase.aa_warfare_systems : 0})</h4>
+          <p>засоби ППО</p></div>
+        <div className={css.td}>
+          <h4>{stats ? stats.planes : '0'}(+{increase ? increase.planes : 0})</h4>
+          <p>літаків</p></div>
+        <div className={css.td}>
+          <h4>{stats ? stats.helicopters : '0'}(+{increase ? increase.helicopters : 0})</h4>
+          <p>РСЗВ</p></div>
+        <div className={css.td}>
+          <h4>{stats ? stats.vehicles_fuel_tanks : '0'}(+{increase ? increase.vehicles_fuel_tanks : 0})</h4>
+          <p>засоби ППО</p></div>
+        <div className={css.td}>
+          <h4>{stats ? stats.warships_cutters : '0'}(+{increase ? increase.warships_cutters : 0})</h4>
+          <p>кораблів</p></div>
+        <div className={css.td}>
+          <h4>{stats ? stats.uav_systems : '0'}(+{increase ? increase.uav_systems : 0})</h4>
+          <p>РСЗВ</p></div>
+        <div className={css.td}>
+          <h4>{stats ? stats.special_military_equip : '0'}(+{increase ? increase.special_military_equip : 0})</h4>
+          <p>спец.техніка</p></div>
+        <div className={css.td}>
+          <h4>{stats ? stats.atgm_srbm_systems : '0'}(+{increase ? increase.atgm_srbm_systems : 0})</h4>
+          <p>установок ОТРК/ТРК</p></div>
+        <div className={css.td}>
+          <h4>{stats ? stats.cruise_missiles : '0'}(+{increase ? increase.cruise_missiles : 0})</h4>
+          <p>крилаті ракети</p></div>
+          <div className={css.td}>
+            <p>бажаєш збільшити стату?</p>
+            <a href="*">Допоможи ЗСУ</a></div>
+        <img className={css.put} width={165} height={185} src={image} alt='123'/>
+        <img className={css.put1} width={150} src={image1} alt='123'/>
+      </div>
+    </div>
+    </div>
+  )
+}
+
+export default App;
